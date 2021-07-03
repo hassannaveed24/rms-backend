a84c744df12fc6e55cc748f8f5bb8ea3371658d8
hassannaveed24 <hassannaveed24@gmail.com>
2021-07-03 19:57:29 +0500

    adding cors policy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
 const port = 8000;
 require('./storage/db')
@@ -10,6 +11,7 @@ const waiter = require("./routes/waiter");
 const order = require("./routes/order");
 
 app.use(express.json());
+app.use(cors())
 // app.use("/cheff", cheff);
 // app.use("/customer", customer);
 // app.use("/meal", meal);
