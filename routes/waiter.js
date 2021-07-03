764aa78820679dba691096fea7679e7af1a0340b
hassannaveed24 <hassannaveed24@gmail.com>
2021-07-03 19:40:30 +0500

    added endpoint for delete waiter

--- routes/waiter.js
+++ routes/waiter.js
@@ -20,17 +20,24 @@ router.get("/", async (req, res) => {
     console.log("/Get Waiters")
     try {
         const db = await connection;
-        const {limit, page} = req.query;
-        if (condition) {
-            
-        } else {
-            const sql = `SELECT * FROM waiters ORDER BY waiter_id LIMIT ? OFFSET ?;`
+        
+        let sql;
+        if (req.query.q) {
+            sql = `SELECT * FROM waiters WHERE name LIKE ? `
+            const [rows, fields] = await db.execute(sql, [req.query.q+"%"])
+            if(rows.length == 0)
+                res.status(204).send()
+            else
+                res.status(200).send(rows)
+        } else if(req.query.limit && req.query.page) {
+            const {limit, page} = req.query;
+            sql = `SELECT * FROM waiters ORDER BY waiter_id LIMIT ? OFFSET ?;`
             const [rows, fields] = await db.execute(sql,[limit, (parseInt(page) * parseInt(limit) ).toString()])
             res.status(200).send(rows)    
         }
         
     } catch (error) {
-        res.status(500).send(error)
+        res.status(500).send({error})
     }
 })
 
@@ -58,8 +65,10 @@ router.patch("/:waiter_id", async (req, res) => {
 router.delete("/:waiter_id", async (req, res) => {
     try {
         const db = await connection;
-
-        
+        console.log("Delete Waiter by id")
+        const sql = `DELETE FROM waiters WHERE waiter_id = ?`
+        await db.execute(sql, [req.params.waiter_id])
+        res.status(200).send()        
     } catch (error) {
         res.status(500).send(error)
     }
